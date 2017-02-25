dabc1bb01fce8d0db8ee9a4277de5bdcadf33cde
ericb <eric_bastarache@hotmail.com>
2017-02-24 19:46:15 -0500

    Updated routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 export const APP_ROUTES: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '',
+    redirectTo: 'home' ,
+    pathMatch: 'full'
+  },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent }
 
 ];
